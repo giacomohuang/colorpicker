3049f060b6b336a1d7d12ffad9d57d4a7a808bb1
Giacomo Huang <giacomohuang@qq.com>
2022-08-24 15:49:07 +0800

    Update package.json&vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { fileURLToPath, URL } from 'url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import { resolve } from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
@@ -13,18 +12,24 @@ export default defineConfig({
   },
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'src/export.js'),
+      // eslint-disable-next-line no-undef
+      entry: resolve(__dirname, 'src/export.js'),
       name: 'ColorPicker',
       fileName: (format) => `colorpicker.${format}.js`,
     },
     rollupOptions: {
-      // 确保外部化处理那些你不想打包进库的依赖
       external: ['vue'],
       output: {
-        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
         globals: {
           vue: 'Vue',
         },
+      },
+    },
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
       },
     },
   },
