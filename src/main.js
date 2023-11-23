0c2a4a7ce049693ca769aae5b0184360539c4dfa
Giacomo Huang <giacomohuang@qq.com>
2023-11-23 15:08:17 +0800

    update docs

--- src/main.js
+++ src/main.js
@@ -1,8 +1,4 @@
 import ColorPicker from './ColorPicker.vue'
-import { createApp } from 'vue'
-import App from './examples/MainView.vue'
-const app = createApp(App)
-app.mount('#app')
 
 ColorPicker.install = function (app) {
   app.component('ColorPicker', ColorPicker)
